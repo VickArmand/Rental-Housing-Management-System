b270cdb246044cf712d187a270f63e90d83c9dd4
victor <victor@blumteq.com>
2025-11-24 17:32:02 +0300

    add a column

--- web/database/migrations/2025_11_18_152057_create_rentals_table.php
+++ web/database/migrations/2025_11_18_152057_create_rentals_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description')->nullable();
             $table->integer('number_of_rooms')->nullable();
             $table->boolean('is_fully_rented')->default(false);
             $table->boolean('is_active')->default(true);
