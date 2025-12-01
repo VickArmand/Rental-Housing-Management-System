2676cd1051157b5d1abbac9ce22b4b23df02ec51
victor <victor@blumteq.com>
2025-12-01 18:10:14 +0300

    add endpoints

--- web/routes/api.php
+++ web/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\CaretakerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::group(['prefix' => '/caretaker'], function() {
+        Route::post('/store', [CaretakerController::class, 'store']);
+    });
+});
