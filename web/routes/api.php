96e205d51da6cac07df415069ec562f067d7d6d0
victor <victor@blumteq.com>
2025-12-03 17:49:33 +0300

    more auth endpoints

--- web/routes/api.php
+++ web/routes/api.php
@@ -18,6 +18,11 @@ use App\Http\Controllers\UserSubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
+Route::post('/reset-password', [UserController::class, 'resetPassword']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
