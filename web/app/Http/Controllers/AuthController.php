df4f1260f064f6c93eab6d4ddfe9bc1f31432d74
victor <victor@blumteq.com>
2026-09-07 11:25:17 +0300

    Creating an Auth controller

---
+++ web/app/Http/Controllers/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Error;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use ReflectionClass;
+
+class AuthController extends Controller
+{
+    //
+    public function userDetails(Request $request)
+    {
+        try {
+            $roleNames = Auth::user()->getRoleNames();
+            $permissions = Auth::user()->getAllPermissions();
+            $roles = Auth::user()->roles;
+            return response()->json(['user' => $request->user(), 'roles' => $roles, 'roleNames' => $roleNames, 'permissions' => $permissions]);
+        } catch (\Exception $e) {
+            Error::saveError('UserController@userDetails', [], (new ReflectionClass($e))->getShortName(), $e->getMessage());
+            return response()->json(['message' => 'Error occurred while fetching user details'], 500);
+        }
+    }
+    public function login(Request $request)
+    {
+        try {
+            $request->validate(
+                [
+                    'email' => 'required',
+                    'password' => 'required|min:8',
+                ]
+            );
+            $user = User::where('email', $request->email)->first();
+            if ($user && Hash::check($request->password, $user->password)) {
+                return $user->createToken('auth_token')->plainTextToken;
+            } else {
+                // Handle the case where the user is not found or credentials are invalid
+                return redirect()->back()->withErrors(['message' => 'Invalid credentials or user not found.']);
+            }
+        } catch (\Exception $e) {
+            Error::saveError('UserController@login', ['email' => $request->email], (new ReflectionClass($e))->getShortName(), $e->getMessage());
+            return response()->json(['message' => 'Error occurred during login'], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->tokens()->delete();
+            return response()->json(['message' => 'Logged out successfully']);
+        } catch (\Exception $e) {
+            Error::saveError('UserController@logout', ['id' => $request->user()->id], (new ReflectionClass($e))->getShortName(), $e->getMessage());
+            return response()->json(['message' => 'Error occurred during logout'], 500);
+        }
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        try {
+            // Implement forgot password logic here
+            $request->validate(['email' => 'required|email']);
+            // Send password reset link or code to the user's email
+            return response()->json(['message' => 'Password reset link sent']);
+        } catch (\Exception $e) {
+            Error::saveError('UserController@forgotPassword', ['email' => $request->email], (new ReflectionClass($e))->getShortName(), $e->getMessage());
+            return response()->json(['message' => 'Error occurred while sending OTP'], 500);
+        }
+    }
+
+    public function resetPassword(Request $request)
+    {
+        try {
+            // Implement reset password logic here
+            $request->validate([
+                'token' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:8|confirmed',
+            ]);
+            // Reset the user's password
+            $user = User::where($request->email)->first();
+            $user->password = Hash::make($request->password);
+            $user->save();
+            return response()->json(['message' => 'Password has been reset']);
+        } catch (\Exception $e) {
+            Error::saveError('UserController@resetPassword', ['email' => $request->email], (new ReflectionClass($e))->getShortName(), $e->getMessage());
+            return response()->json(['message' => 'Error occurred during password reset'], 500);
+        }
+    }
+}
--- web/routes/api.php
+++ web/routes/api.php
@@ -14,17 +14,18 @@ use App\Http\Controllers\TenantController;
 use App\Http\Controllers\TenantReminderController;
 use App\Http\Controllers\TenantReminderStatusController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserSubscriptionController;
 use App\Http\Controllers\ErrorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
-Route::post('/reset-password', [UserController::class, 'resetPassword']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [UserController::class, 'userDetails'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/user', [AuthController::class, 'userDetails'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::group(['prefix' => '/caretakers'], function() {
